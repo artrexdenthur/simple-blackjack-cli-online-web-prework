f910bbd9806f8e3f33b704a5aacd5b00c95a0f37
Paul Ashour <paul.ashour@gmail.com>
2018-10-01 04:35:59 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -8,29 +8,47 @@ def deal_card
   rand(1..11)
 end
 
-def display_card_total
+def display_card_total(num)
   # code #display_card_total here
-  
+  puts "Your cards add up to #{num}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
+def end_game(num)
   # code #end_game here
+  "Sorry, you hit #{num}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  # deal two cards
+  total = deal_card
+  total += deal_card
+  # display then return the total
+  display_card_total(total)
+  total
 end
 
-def hit?
+def hit?(total)
   # code hit? here
+  loop do
+    prompt_user
+    choice = get_user_input
+    case choice
+    when 'h'
+      deal_card
+    when 's'
+        
+  end
 end
 
 def invalid_command
